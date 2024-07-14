a46b81422ab7649ecb4121487faa58601c4082d8
mohamedtarikz <mohamedtarik06@gmail.com>
2024-07-14 15:19:17 +0300

    Just Started!

---
+++ main.cpp
@@ -0,0 +1,17 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+class Teacher{
+    double rate;
+    string name;
+    int experience;
+
+};
+
+class Course{
+    int price;
+};
+
+int main() {
+    return 0;
+}
