9824ea66ba15c22295254f596a805943130fa2ad
mohamedtarikz <mohamedtarik06@gmail.com>
2024-08-01 15:30:56 +0300

    Seperated Classes and main code --Draft

--- main.cpp
+++ main.cpp
@@ -6,41 +6,9 @@
 using namespace std;
 using namespace course;
 
-string day[7] = {"Sun","Mon","Tue","Wed","Thu","Fri","Sat"};
 
 //initializing sample data for testing
-void Init_Instructors(){
-    //initialize instructors
-    Instructors.emplace_back("T12","Samah Abd-ElWahed",4.65,5);
-    Instructors.emplace_back("T01","Adel Shakal",5.0,9);
-    Instructors.emplace_back("T45","Samar Shakshouka",3.8,3);
-    Instructors.emplace_back("T06","Amr Elsisi",2.9,6);
-    Instructors.emplace_back("T10","Alaa Mubarak",4.5,10);
-}
-void Init_Courses(){
-    //initialize courses
-    Courses.emplace_back("CS101","Introduction to Computer Science",249.99);
-    Courses[0].AddInstructor(Instructors[0]);
-    Courses[0].AddInstructor(Instructors[2]);
-    Courses[0].AddDays(day[0]);
-    Courses[0].AddDays(day[4]);
-    Courses.emplace_back("IT101","Introduction to Networks",549.99);
-    Courses[1].AddInstructor(Instructors[1]);
-    Courses[1].AddInstructor(Instructors[2]);
-    Courses[1].AddDays(day[1]);
-    Courses[1].AddDays(day[3]);
-    Courses.emplace_back("AI201","Learning about Neural Networks",999.99);
-    Courses[2].AddInstructor(Instructors[3]);
-    Courses[2].AddInstructor(Instructors[4]);
-    Courses[2].AddInstructor(Instructors[0]);
-    Courses[2].AddDays(day[5]);
-    Courses[2].AddDays(day[6]);
-    Courses.emplace_back("MA121","Partial Differential Equation",749.99);
-    Courses[3].AddInstructor(Instructors[1]);
-    Courses[3].AddInstructor(Instructors[4]);
-    Courses[3].AddDays(day[0]);
-    Courses[3].AddDays(day[2]);
-}
+
 
 int main() {
     ////////////     Instructors     //////////////////
